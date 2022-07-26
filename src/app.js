11c7c3640cea5748d1de3f2e83b11468c7195c15
Om Prakash Prajapati <omprakash.prajapati@habilelabs.io>
2022-07-26 12:08:12 +0530

    feat: added cors middleware

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(require("./middlewares/response-handler.middleware"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("./middlewares/cors"));
 
 app.use(cors({
     origin: "*",
